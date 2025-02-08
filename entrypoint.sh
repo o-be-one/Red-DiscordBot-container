f5176c4d4545b37445f4993b3de504ff06d39476
o-be-one <github@btrcloud.com>
2025-02-08 03:52:47 -0500

    fix(entrypoint): allow missing token so it loads from config

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,9 @@ if [ ! -f "/home/redbot/.config/config.json" ]; then
         --instance-name "${REDBOT_NAME}"
 fi
 
-exec redbot "${REDBOT_NAME}" --no-prompt --token "${REDBOT_TOKEN}" --prefix "${REDBOT_PREFIX}" --owner "${REDBOT_OWNER}" "$@"
+if [ -z "${REDBOT_TOKEN}" ]; then
+    exec redbot "${REDBOT_NAME}" --no-prompt --prefix "${REDBOT_PREFIX}" --owner "${REDBOT_OWNER}" ${REDBOT_ARGS}
+else
+    exec redbot "${REDBOT_NAME}" --no-prompt --token "${REDBOT_TOKEN}" --prefix "${REDBOT_PREFIX}" --owner "${REDBOT_OWNER}" ${REDBOT_ARGS}
+fi
+
